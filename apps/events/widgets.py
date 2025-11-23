6a6ec700302eaac74a765aa0cca8c3816d0a9aa7
Charles_Nguenkam <mignoncharly@yahoo.fr>
2025-11-23 19:17:01 +0100

    Improve error handling in GalleryImageWidget
    Refactor error handling in GalleryImageWidget to log specific exceptions for better debugging.

--- apps/events/widgets.py
+++ apps/events/widgets.py
@@ -1,54 +1,71 @@
 # apps/events/widgets.py
+import logging
 
-from django import forms
-from django.utils.translation import gettext_lazy as _
-from django.template.loader import render_to_string
-from django.utils.safestring import mark_safe
+from django.core.exceptions import ObjectDoesNotExist
 from django.forms.widgets import RadioSelect
+from django.utils.translation import gettext_lazy as _
+
+logger = logging.getLogger(__name__)
 
 
 class GalleryImageWidget(RadioSelect):
     """Custom widget for selecting images from a gallery."""
+
     template_name = 'events/widgets/gallery_image_widget.html'
     option_template_name = 'events/widgets/gallery_image_option.html'
-    
+
     def __init__(self, attrs=None, image_type='all'):
         super().__init__(attrs)
         self.image_type = image_type
-    
+
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
         context['widget']['image_type'] = self.image_type
-        
+
         # Group options by folder
         grouped_options = {'all': [], 'event': [], 'category': [], 'item': []}
-        
+
         for group, options, index in context['widget']['optgroups']:
             for option in options:
                 # Skip empty option (for "Upload your own")
                 if not option['value']:
                     continue
-                
+
                 # Get image type from the value (would come from the queryset)
                 try:
                     from .models import PredefinedImage
+
                     image = PredefinedImage.objects.get(id=option['value'])
                     image_type = image.image_type
                     option['image'] = image
-                    
+
                     # Add to appropriate group
                     if image_type == 'all':
                         grouped_options['all'].append(option)
                     else:
                         grouped_options[image_type].append(option)
-                        
-                except Exception:
-                    # If error, just add to all
+
+                except ObjectDoesNotExist:
+                    logger.warning(
+                        "PredefinedImage with id=%s not found, adding to 'all' group",
+                        option['value']
+                    )
                     grouped_options['all'].append(option)
-        
+                except (ValueError, TypeError) as e:
+                    logger.warning(
+                        "Invalid option value %s: %s",
+                        option['value'],
+                        e
+                    )
+                    grouped_options['all'].append(option)
+
         context['widget']['grouped_options'] = grouped_options
-        context['widget']['has_folders'] = any(len(opts) > 0 for folder, opts in grouped_options.items() if folder != 'all')
-        
+        context['widget']['has_folders'] = any(
+            len(opts) > 0
+            for folder, opts in grouped_options.items()
+            if folder != 'all'
+        )
+
         return context
 
 
