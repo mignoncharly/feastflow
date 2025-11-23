fe3d6ed049a7c54952b1819c14ce6e3a5b61dec7
Charles_Nguenkam <mignoncharly@yahoo.fr>
2025-11-23 19:13:32 +0100

    Refactor Django URL configuration and debug toolbar
    Refactor URL patterns and improve debug toolbar integration.

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,15 @@
 # config/urls.py
 
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
-from django.utils.translation import gettext_lazy as _
-import debug_toolbar
-
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     # URLs that shouldn't have language prefix
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
-    # Add other non-i18n URLs here
 ]
 
 # URLs with language prefix
@@ -22,15 +18,21 @@ urlpatterns += i18n_patterns(
     path('events/', include('apps.events.urls')),
     path('contributions/', include('apps.contributions.urls')),
     path('accounts/', include('apps.accounts.urls')),
-    # Add other i18n URLs here
-    prefix_default_language=False,  # Don't add prefix for default language
+    prefix_default_language=False,
 )
 
-# Serve media files in development
+# Serve media and static files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    path('__debug__/', include(debug_toolbar.urls)),
+
+    # Debug toolbar (only if installed and in INSTALLED_APPS)
+    if 'debug_toolbar' in settings.INSTALLED_APPS:
+        import debug_toolbar
+
+        urlpatterns = [
+            path('__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
 
 
 handler404 = 'apps.core.views.handle_404'
