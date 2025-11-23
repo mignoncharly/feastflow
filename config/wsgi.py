a75d59d6a23a104875aac30cb8c9cf9b479861bc
Charles_Nguenkam <mignoncharly@yahoo.fr>
2025-11-23 19:12:58 +0100

    Modify WSGI config for FeastFlow project
    Updated project name and documentation for WSGI config.

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,27 @@
 """
-WSGI config for event_tracker project.
+WSGI config for FeastFlow project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+
+Usage:
+    - For local development: Set DJANGO_SETTINGS_MODULE=config.settings.settings_local
+    - For PythonAnywhere: Set DJANGO_SETTINGS_MODULE=config.settings.settings_pythonanywhere
+
+The settings module MUST be set via environment variable before this file is loaded.
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_tracker.settings')
+# Default to local settings if not specified
+# In production (PythonAnywhere), set this in the WSGI configuration file
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'config.settings.settings_local'
+)
 
 application = get_wsgi_application()
