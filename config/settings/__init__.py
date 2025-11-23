9cfd06e0cac1ff8df8b9210b6a53dfd718a2de6e
Charles_Nguenkam <mignoncharly@yahoo.fr>
2025-11-23 19:10:52 +0100

    Create __init__.py for Django settings package
    Added documentation for available settings modules and usage.

---
+++ config/settings/__init__.py
@@ -0,0 +1,13 @@
+# config/settings/__init__.py
+"""
+Django settings package for FeastFlow.
+
+Available settings modules:
+- settings_local: For local development
+- settings_pythonanywhere: For PythonAnywhere deployment
+
+Usage:
+    Set DJANGO_SETTINGS_MODULE environment variable to one of:
+    - config.settings.settings_local
+    - config.settings.settings_pythonanywhere
+"""
