43dee9b64201f2ddb0a0fa8771e1f08e5e428355
Charles_Nguenkam <mignoncharly@yahoo.fr>
2025-11-23 19:11:23 +0100

    Change Django settings module to settings_local

--- manage.py
+++ manage.py
@@ -6,13 +6,18 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    # Set the default settings module to config.settings.development
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
+    # Default to local development settings
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'config.settings.settings_local'
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed?"
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
 
