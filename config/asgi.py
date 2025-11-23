bd5bb5304c86b8a57791408a004499f37986661e
Charles_Nguenkam <mignoncharly@yahoo.fr>
2025-11-23 19:12:29 +0100

    Modify ASGI config for FeastFlow project
    Updated project name and ASGI settings usage instructions.

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,26 @@
 """
-ASGI config for event_tracker project.
+ASGI config for FeastFlow project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+
+Usage:
+    - For local development: Set DJANGO_SETTINGS_MODULE=config.settings.settings_local
+    - For PythonAnywhere: Set DJANGO_SETTINGS_MODULE=config.settings.settings_pythonanywhere
+
+The settings module MUST be set via environment variable before this file is loaded.
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_tracker.settings')
+# Default to local settings if not specified
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'config.settings.settings_local'
+)
 
 application = get_asgi_application()
